0fb21c32dafaae567ab5ec8d4cba445707ad69c6
ycourses <cood_ip@hotmail.com>
2018-05-03 20:58:23 +0200

    update index file

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <html><head><title></title></head><body>
-	<h1>AQS AJAX LIB </h1>
+	<h1>AQS AJAX LIB 2018</h1>
+	<span> Developer by : adham allam  </span>
+	<span> lib function   </span>
 	
 </body>	</html>
