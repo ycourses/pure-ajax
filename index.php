4a69365009b6f62ad4c10b79050c200769f28335
ycourses <cood_ip@hotmail.com>
2018-05-17 16:40:02 +0200

    up date index ex 1

--- index.php
+++ index.php
@@ -1,6 +1,33 @@
-<html><head><title></title></head><body>
-	<h1>AQS AJAX LIB 2018</h1>
-	<span> Developer by : adham allam  </span>
-	<span> lib function   </span>
-	
-</body>	</html>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>AQs puer javascript Lib</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="scr/AQs.js"></script><!-- JQ admin-->
+</head>
+<body>
+<h1>AQs Puer javascript ajax Lib </h1>
+<input required="" type="text" class="form-control" name="Username" id="Username" placeholder="userName"   value="">
+<div id="result"></div>
+<div id="result2"></div>
+
+<script type="text/javascript">
+    var axjson = $.$("#Username");
+    var result = $.$("#result");
+   // var result2 = $.$("#result2");
+    var url='http://www.scv.net/users/chackUserjax/';
+
+    axjson.addEventListener("blur", function(){
+        'use strict';
+        $.AQs({type: 'post', url: url, data: '?Username='+this.value,imsg:0, sucs: function (Msg) {
+                result.innerHTML=Msg;
+                $.$("#result2").innerHTML = Msg;
+            }
+
+        });
+    });
+</script>
+</body>
+</html>
